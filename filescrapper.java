b8a11baf366244961b8a2d2402cf9e11299b2e3f
Aasim <aasimrazajatoi@gmail.com>
2021-10-23 18:57:27 +0500

    added code to list

--- filescrapper.java
+++ filescrapper.java
@@ -3,5 +3,12 @@ class HelloWorld {
     public static void main(String[] args) {
         // creates a file object
     File file = new File("C:\\Users\\Guest User\\Desktop\\Java File\\List Method"); 
+
+    // returns an array of all files
+    String[] fileList = file.list();
+
+    for(String str : fileList) {
+      System.out.println(str);
     }
+}
 }
