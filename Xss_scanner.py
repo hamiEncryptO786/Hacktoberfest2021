9df64b4b9679d4e68e9b1647a1e72c3c033e2618
root <root@localhost.localdomain>
2021-10-28 12:48:48 +0500

    Lines added to catch action, method and various input attributes

--- Xss_scanner.py
+++ Xss_scanner.py
@@ -7,3 +7,15 @@ def get_all_forms(url):
     """Given a `url`, it returns all forms from the HTML content"""
     soup = bs(requests.get(url).content, "html.parser")
     return soup.find_all("form")
+
+def get_form_details(form):
+    """
+    This function extracts all possible useful information about an HTML `form`
+    """
+    details = {}
+    # get the form action (target url)
+    action = form.attrs.get("action").lower()
+    # get the form method (POST, GET, etc.)
+    method = form.attrs.get("method", "get").lower()
+    # get all the input details such as type and name
+    inputs = []
