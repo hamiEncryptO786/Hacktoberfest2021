593825fde96fc0cc2e9bd0b5413074ee438ca347
root <root@localhost.localdomain>
2021-10-28 12:37:02 +0500

    XSS Function for Further projects

---
+++ Function_of_XSS_Scanner.py
@@ -0,0 +1,4 @@
+def get_all_forms(url):
+    """Given a `url`, it returns all forms from the HTML content"""
+    soup = bs(requests.get(url).content, "html.parser")
+    return soup.find_all("form")
