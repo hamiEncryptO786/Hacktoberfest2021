a5f1d110045b1a136e913e2207f4a28f128a7899
root <root@localhost.localdomain>
2021-10-28 12:57:35 +0500

    The above function takes form details which is the output of get form details

--- Xss_scanner.py
+++ Xss_scanner.py
@@ -19,3 +19,34 @@ def get_form_details(form):
     method = form.attrs.get("method", "get").lower()
     # get all the input details such as type and name
     inputs = []
+    
+    def submit_form(form_details, url, value):
+    """
+    Submits a form given in `form_details`
+    Params:
+        form_details (list): a dictionary that contain form information
+        url (str): the original URL that contain that form
+        value (str): this will be replaced to all text and search inputs
+    Returns the HTTP Response after form submission
+    """
+    # construct the full URL (if the url provided in action is relative)
+    target_url = urljoin(url, form_details["action"])
+    # get the inputs
+    inputs = form_details["inputs"]
+    data = {}
+    for input in inputs:
+        # replace all text and search values with `value`
+        if input["type"] == "text" or input["type"] == "search":
+            input["value"] = value
+        input_name = input.get("name")
+        input_value = input.get("value")
+        if input_name and input_value:
+            # if input name and value are not None, 
+            # then add them to the data of form submission
+            data[input_name] = input_value
+
+    if form_details["method"] == "post":
+        return requests.post(target_url, data=data)
+    else:
+        # GET request
+        return requests.get(target_url, params=data)
