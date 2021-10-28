eeee00fd399e27cec2845dd62cd7a234961151ed
root <root@localhost.localdomain>
2021-10-28 12:41:51 +0500

    Further modified the xss_scanner code for functionalities

--- Xss_scanner.py
+++ Xss_scanner.py
@@ -2,3 +2,8 @@ import requests
 from pprint import pprint
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
+
+def get_all_forms(url):
+    """Given a `url`, it returns all forms from the HTML content"""
+    soup = bs(requests.get(url).content, "html.parser")
+    return soup.find_all("form")
